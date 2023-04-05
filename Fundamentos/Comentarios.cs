52fbb5a880e1316c92c076674d8122eb93c52773
Emiliano23atr <joaovitoremiliano5@gmail.com>
2023-04-05 14:47:59 -0300

    Comentarios

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            // Cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            Console.WriteLine("O C# tem o XML Comments");
+            /*
+
+            */
+        }
+    }
+}
